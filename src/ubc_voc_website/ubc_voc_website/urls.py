f9ac3f6cbc587e12ad696ad6f4d6c83484c0642c
Emily Wood <emily.wood1624@gmail.com>
2024-06-15 19:08:33 -0700

    signup form

--- src/ubc_voc_website/ubc_voc_website/urls.py
+++ src/ubc_voc_website/ubc_voc_website/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from .views import join
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include("django.contrib.auth.urls")),
+    path('join/', join, name='join'),
+    path('membership/', include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home")
 ]
