bfc7b3ed5ad9cde26c14333cfa37bc6e67a3e527
Emily Wood <emily.wood1624@gmail.com>
2024-06-15 23:06:19 -0700

    add profile page

--- src/ubc_voc_website/membership/views.py
+++ src/ubc_voc_website/membership/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-# Create your views here.
+@login_required
+def profile(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    return render(request, 'profile.html', {'user': user, 'profile': profile})
--- src/ubc_voc_website/ubc_voc_website/urls.py
+++ src/ubc_voc_website/ubc_voc_website/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from .views import join
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('join/', join, name='join'),
-    path('membership/', include("django.contrib.auth.urls")),
+    path('accounts/join/', join, name='join'),
+    path('accounts/', include("django.contrib.auth.urls")),
+    path("membership/", include("membership.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home")
 ]
