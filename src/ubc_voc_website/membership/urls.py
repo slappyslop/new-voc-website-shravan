743fe3064fb86dab9dd6ac9ea66a5c7f1b547c0e
Emily Wood <emily.wood1624@gmail.com>
2024-07-17 20:54:36 -0700

    create basic membership_admin views

--- src/ubc_voc_website/membership/urls.py
+++ src/ubc_voc_website/membership/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import profile
+from .views import *
 
 urlpatterns = [ 
-    path("profile/", profile, name="profile")
+    path("member-list", member_list, name="member_list"),
+    path("profile/", profile, name="profile"),
+    path("my-profile", my_profile, name="my_profile"),
+    path("edit-user", edit_user_profile, name="edit_user"),
+    path("manage-roles", manage_roles, name="manage_roles"),
+    path("membership-stats", member_list, name="membership_stats")
 ]
--- src/ubc_voc_website/membership/views.py
+++ src/ubc_voc_website/membership/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Profile
+from django.db.models import OuterRef, Subquery
+from .models import Exec, Membership, Profile
+from ubc_voc_website.decorators import Members, Execs
 
-@login_required
-def profile(request):
+@Members
+def member_list(request):
+    profiles = Profile.objects.all().filter(user__is_active=True)
+    return profiles
+
+@Members
+def profile(request, user):
+    profile = Profile.objects.get(user=user)
+    return render(request, 'profile.html', {'user': user, 'profile': profile})
+
+@Members
+def my_profile(request):
     user = request.user
     profile = Profile.objects.get(user=user)
     return render(request, 'profile.html', {'user': user, 'profile': profile})
+
+@Members
+def edit_user_profile(request, user):
+    profile = Profile.objects.get(user=user)
+    return render(request, 'profile.html', {'user': user, 'profile': profile})
+
+@Execs
+def manage_roles(request):
+    execs = Exec.objects.filter(user=OuterRef('user'))
+    profiles = Profile.objects.all().filter(user__in=Subquery(execs.values('user')))
+    return profiles
+
+@Execs
+def membership_stats(request):
+    num_inactive_accounts = Profile.objects.all().filter(user__is_active=False).count()
+    num_regular_members = Membership.objects.all().filter(type=Membership.MembershipType.REGULAR).count()
+    num_associate_members = Membership.objects.all().filter(type=Membership.MembershipType.ASSOCIATE).count()
+    num_honourary_members = Membership.objects.all().filter(type=Membership.MembershipType.HONORARY).count()
+
+    # TODO add more stats that would be useful/interesting (trip signups, etc.)
+
+    return (num_inactive_accounts, num_regular_members, num_associate_members, num_honourary_members)
