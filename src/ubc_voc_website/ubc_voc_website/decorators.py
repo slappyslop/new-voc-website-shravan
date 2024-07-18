9f9d453ac7f96dad27f138a7a7756493f823a424
Emily Wood <emily.wood1624@gmail.com>
2024-07-17 20:54:16 -0700

    decorators for exec, members, psg

---
+++ src/ubc_voc_website/ubc_voc_website/decorators.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
+def Members(view_function):
+    @login_required
+    def _view(request, *args, **kwargs):
+        if request.user.groups.filter(name='Members').exists():
+            return view_function(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return _view
+
+def Execs(view_function):
+    @login_required
+    def _view(request, *args, **kwargs):
+        if request.user.groups.filter(name='Exec').exists():
+            return view_function(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return _view
+
+def PSG(view_function):
+    @login_required
+    def _view(request, *args, **kwargs):
+        if request.user.groups.filter(name='PSG').exists():
+            return view_function(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return _view
+
+
