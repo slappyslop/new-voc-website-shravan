89484200b9abe410622831dd7458d9540fd31b92
Emily Wood <emily.wood1624@gmail.com>
2024-08-24 20:21:12 -0700

    membership form

---
+++ src/ubc_voc_website/membership/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from .models import Membership
+
+from .utils import *
+import datetime
+
+class MembershipForm(forms.ModelForm):
+    class Meta:
+        model = Membership
+        fields = ('start_date', 'end_date', 'type')
+
+    start_date = forms.DateField(
+        initial=datetime.datetime.today(),
+        required=True,
+        disabled=True
+    )
+    end_date = forms.DateField(
+        initial=get_end_date(datetime.datetime.today()),
+        required=True,
+        disabled=True
+    )
+    type = forms.ChoiceField(
+        choices=Membership.MembershipType.choices, 
+        required=True,
+        initial=Membership.MembershipType.REGULAR
+    )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Any view that renders this form will need to pass in the current request.user to be used by the form
+        This eliminates the need for 'user' to be a disabled field on the form
+        """
+        self.user = kwargs.pop('user', None)
+        super(MembershipForm, self).__init__(*args, **kwargs)
+        self.fields['type'].label = "Membership Type"
+
+    def save(self, commit=True):
+        membership = super(MembershipForm, self).save(commit=False)
+        if self.user:
+            membership.user = self.user
+            if commit:
+                membership.save()
+        return membership
--- src/ubc_voc_website/membership/urls.py
+++ src/ubc_voc_website/membership/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [ 
+    path('apply', apply, name="apply"),
     path("members", member_list, name="members"),
     path("profile/<int:id>", profile, name="profile"),
     path("my-profile", my_profile, name="my_profile"),
