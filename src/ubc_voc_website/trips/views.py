e61b41100caed020a6068132b85cd39415da27fd
epw1624 <emily.wood1624@gmail.com>
2024-11-17 15:37:17 -0800

    skeletons for trip app views

--- src/ubc_voc_website/trips/views.py
+++ src/ubc_voc_website/trips/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from ubc_voc_website.decorators import Admin, Members, Execs
+
+def trip_agenda(request):
+    # calendar and list view of all published trips
+    pass
+
+@Members
+def my_trips(request):
+    # base page for all trips, can navigate to create/edit pages from here
+    pass
+
+@Members
+def create_trip(request):
+    pass
+
+@Members
+def edit_trip(request):
+    pass
+
+@Members
+def view_trip(request):
+    pass
