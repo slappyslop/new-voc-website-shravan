00be9f76e20690fc414b1c2090ea31eacedfd5d4
epw1624 <emily.wood1624@gmail.com>
2025-06-04 22:33:25 -0700

    trip gallery feature

--- src/ubc_voc_website/gallery/forms.py
+++ src/ubc_voc_website/gallery/forms.py
@@ -2,11 +2,15 @@ from django import forms
 
 from photologue.models import Photo
 
-class UserPhotoUploadForm(forms.ModelForm):
+class PhotoUploadForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ['title', 'image', 'caption']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class UserPhotoUploadForm(PhotoUploadForm):
     def __init__(self, *args, user=None, **kwargs):
         self.user = user
         super().__init__(*args, *kwargs)
@@ -18,4 +22,18 @@ class UserPhotoUploadForm(forms.ModelForm):
         if Photo.objects.filter(title=full_title).exists():
             raise forms.ValidationError("You already have a photo with this title")
         
+        return full_title
+    
+class TripPhotoUploadForm(PhotoUploadForm):
+    def __init__(self, *args, trip=None, **kwargs):
+        self.trip = trip
+        super().__init__(*args, **kwargs)
+
+    def clean_title(self):
+        original_title = self.cleaned_data['title']
+        full_title = f"trip{self.trip.id}:{original_title}"
+
+        if Photo.objects.filter(title=full_title).exists():
+            raise forms.ValidationError("The trip already has a photo with this title")
+
         return full_title
--- src/ubc_voc_website/gallery/views.py
+++ src/ubc_voc_website/gallery/views.py
@@ -3,7 +3,7 @@ import os
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.text import slugify
 
-from .forms import UserPhotoUploadForm
+from .forms import TripPhotoUploadForm, UserPhotoUploadForm
 from .models import TripGallery, UserGallery
 from trips.models import Trip, TripSignup, TripSignupTypes
 from photologue.models import Photo
@@ -63,19 +63,39 @@ def manage_trip_gallery(request, trip_id):
     if not going_list.filter(user=request.user).exists():
         return render(request, 'access_denied.html', status=403)
     
-    for gallery in TripGallery.objects.all():
-        print(gallery) 
-    
-    trip_name = f"Trip Gallery: {trip.name}"
+    gallery_name = f"Trip Gallery: {trip.name}"
     gallery, created = TripGallery.objects.get_or_create(
         trip=trip,
         defaults={
-            'title': trip_name,
-            'slug': slugify(trip_name),
+            'title': gallery_name,
+            'slug': slugify(gallery_name),
             'is_public': True
         }
     )
 
+    if request.method == 'POST':
+        form = TripPhotoUploadForm(request.POST, request.FILES, trip=trip)
+        if form.is_valid():
+            photo = form.save(commit=False)
+
+            # Update file path to save in folder for this trip
+            _, ext = os.path.splitext(form.cleaned_data['image'].name)
+            new_path = f"trip/{trip_id}/{slugify(form.cleaned_data['title'].split(':', 1))}{ext}"
+            photo.image.name = new_path
+
+            # Update Photo Fields
+            photo.slug = slugify(photo.title)
+            photo.is_public = True
+            photo.save()
+
+            gallery.photos.add(photo)
+
+            return redirect('manage_trip_gallery', trip_id=trip_id)
+        
+    else:
+        form = TripPhotoUploadForm(trip=trip)
+
     return render(request, 'gallery/gallery.html', {
         'gallery': gallery,
+        'form': form
     })
