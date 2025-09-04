a93b279bc101d695c1628983474abb3c0f6ac0be
epw1624 <emily.wood1624@gmail.com>
2025-09-04 11:24:21 -0700

    trip report comments

--- src/ubc_voc_website/tripreports/models.py
+++ src/ubc_voc_website/tripreports/models.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.db import models
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
@@ -10,6 +14,40 @@ class TripReport(Page):
     ]
 
     parent_page_types = ['tripreports.TripReportIndexPage']
+
+    def serve(self, request):
+        from .forms import CommentForm
+
+        if request.method == "POST":
+            if request.user.is_authenticated:
+                form = CommentForm(request.POST)
+                if form.is_valid():
+                    comment = form.save(commit=False)
+                    comment.trip_report = self
+                    comment.user = request.user
+                    comment.save()
+                    return redirect(self.url)       
+        else:
+            form = CommentForm()
+
+        context = super().get_context(request)
+        context["form"] = form
+        context["comments"] = self.comments.all()
+
+        return TemplateResponse(request, self.get_template(request), context)
+
+class Comment(models.Model):
+    trip_report = models.ForeignKey(
+        TripReport,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT
+    )
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 class TripReportIndexPage(Page):
     intro = RichTextField(blank=True)
