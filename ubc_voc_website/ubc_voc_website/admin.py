0a53bcbbc84c17dc8fee96186228b30e9f67ae8f
epw1624 <emily.wood1624@gmail.com>
2025-10-25 12:31:03 -0700

    add user model to admin

--- ubc_voc_website/ubc_voc_website/admin.py
+++ ubc_voc_website/ubc_voc_website/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
-admin.site.unregister(Group)
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login',)}),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
