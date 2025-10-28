7f02cc984d3645e4e3f65054b048a977e99d7e69
epw1624 <emily.wood1624@gmail.com>
2025-10-27 22:06:38 -0700

    add profile to admin

--- ubc_voc_website/membership/admin.py
+++ ubc_voc_website/membership/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Membership
+from .models import Membership, Profile
 
 @admin.register(Membership)
 class MembershipAdmin(admin.ModelAdmin):
     list_display = ('user', 'start_date', 'end_date', 'type')
     search_fields = ('user',)
     list_filter = ('start_date', 'end_date')
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone')
+    search_fields = ('user',)
+    list_filter = ('first_name', 'last_name')
