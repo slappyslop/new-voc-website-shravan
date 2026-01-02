04154b70b78e6d2b982825435627012d1b0c03e7
epw1624 <emily.wood1624@gmail.com>
2026-01-01 21:09:01 -0800

    switch to csv migrations

--- ubc_voc_website/ubc_voc_website/management/commands/migrate_user.py
+++ ubc_voc_website/ubc_voc_website/management/commands/migrate_user.py
@@ -1,29 +1,25 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-import pymysql
-import os
+import csv
+
+User = get_user_model()
 
 class Command(BaseCommand):
-    help="Migrate Users from old database"
+    help="Migrate Users from CSV"
 
     def handle(self, *args, **options):
-        User = get_user_model()
+        path="data_migration/users.csv"
 
-        mysql_conn = pymysql.connect(
-            host=os.getenv("OLD_DB_HOST"),
-            user=os.getenv("OLD_DB_USER"),
-            password=os.getenv("OLD_DB_PASSWORD"),
-            database=os.getenv("OLD_DB_DATABASE"),
-            charset="utf8mb4",
-            cursorclass=pymysql.cursors.DictCursor
-        )
+        with open(path, newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f, fieldnames=["id", "email"])
 
-        with mysql_conn.cursor() as cursor:
-            cursor.execute("SELECT id, email FROM members;")
-            for row in cursor.fetchall():
+            for row in reader:
                 user, created = User.objects.get_or_create(
-                    email=row['email'],
-                    defaults={'old_id': row['id']}
+                    email=row['email'].strip(),
+                    defaults={
+                        'old_id': int(row["id"]),
+                        'is_active': True
+                    }
                 )
                 if created:
                     user.set_password(User.objects.make_random_password())
@@ -32,5 +28,4 @@ class Command(BaseCommand):
                 else:
                     self.stdout.write(f"User {user.email} already exists")
 
-        mysql_conn.close()
-        self.stdout.write(self.style.SUCCESS("User migration complete"))
+        self.stdout.write(self.style.SUCCESS("User migration complete"))        
