9034fee9871f4587a06eeecbe2a35c531071d943
epw1624 <emily.wood1624@gmail.com>
2026-01-02 15:40:04 -0800

    catch missing user errors

--- ubc_voc_website/membership/management/commands/migrate_profile.py
+++ ubc_voc_website/membership/management/commands/migrate_profile.py
@@ -17,44 +17,45 @@ class Command(BaseCommand):
             reader = csv.DictReader(f, fieldnames=["id", "firstname", "lastname", "phone", "blurb", "birthdate", "acc", "studentnumber", "vocene", "emergency_info", "spot_url", "trip_email", "pronouns"])
 
             for row in reader:
-                user = User.objects.get(old_id=int(row['id']))
-
-                if not user:
+                try:
+                    user = User.objects.get(old_id=int(row['id']))
+                except User.DoesNotExist:
                     self.stdout.write(self.style.WARNING(f"User not found for old_id {int(row['id'])}"))
+                    continue
+                
+                try:
+                    birthdate = datetime.strptime(row['birthdate'], "%Y-%m-%d").date()
+                except ValueError:
+                    birthdate = date(1970, 1, 1)
+                profile, created = Profile.objects.get_or_create(
+                    user=user,
+                    defaults={
+                        'first_name': row['firstname'],
+                        'last_name': row['lastname'],
+                        'phone': row['phone'],
+                        'birthdate': birthdate
+                    }
+                )
+                profile.pronouns = row['pronouns']
+                profile.student_number = row['studentnumber']
+                profile.bio = row['blurb']
+                profile.emergency_info = row['emergency_info']
+                profile.inreach_address = row['spot_url']
+                profile.acc = row['acc'] == "1"
+                profile.vocene = row['vocene'] == "1"
+                profile.trip_org_email = row['trip_email'] == "1"
+
+                if not created:
+                    profile.first_name = row['firstname']
+                    profile.last_name = row['lastname']
+                    profile.phone = row['phone']
+                    profile.birthdate = birthdate
+
+                profile.save()
+
+                if created:
+                    self.stdout.write(self.style.SUCCESS(f"Created profile for user {user.email}"))
                 else:
-                    try:
-                        birthdate = datetime.strptime(row['birthdate'], "%Y-%m-%d").date()
-                    except ValueError:
-                        birthdate = date(1970, 1, 1)
-                    profile, created = Profile.objects.get_or_create(
-                        user=user,
-                        defaults={
-                            'first_name': row['firstname'],
-                            'last_name': row['lastname'],
-                            'phone': row['phone'],
-                            'birthdate': birthdate
-                        }
-                    )
-                    profile.pronouns = row['pronouns']
-                    profile.student_number = row['studentnumber']
-                    profile.bio = row['blurb']
-                    profile.emergency_info = row['emergency_info']
-                    profile.inreach_address = row['spot_url']
-                    profile.acc = row['acc'] == "1"
-                    profile.vocene = row['vocene'] == "1"
-                    profile.trip_org_email = row['trip_email'] == "1"
-
-                    if not created:
-                        profile.first_name = row['firstname']
-                        profile.last_name = row['lastname']
-                        profile.phone = row['phone']
-                        profile.birthdate = birthdate
-
-                    profile.save()
-
-                    if created:
-                        self.stdout.write(self.style.SUCCESS(f"Created profile for user {user.email}"))
-                    else:
-                        self.stdout.write(f"Profile for user {user.email} already exists")
+                    self.stdout.write(f"Profile for user {user.email} already exists")
 
             self.stdout.write(self.style.SUCCESS("Profile migration complete"))
