291bcfbb60b4b9b2a6998901366fd8f3e4e89b34
epw1624 <emily.wood1624@gmail.com>
2026-01-04 16:43:24 -0800

    add first and last name fields to exec and psg admin

--- ubc_voc_website/membership/admin.py
+++ ubc_voc_website/membership/admin.py
@@ -23,8 +23,20 @@ class ExecAdmin(admin.ModelAdmin):
     autocomplete_fields = ('user',)
     search_fields = ('user__email', 'user__profile__first_name', 'user__profile__last_name', 'exec_role')
 
+    def first_name(self, obj):
+        return obj.user.profile.first_name
+
+    def last_name(self, obj):
+        return obj.user.profile.last_name
+
 @admin.register(PSG)
 class PSGAdmin(admin.ModelAdmin):
     list_display = ('user', 'first_name', 'last_name')
     autocomplete_fields = ('user',)
     search_fields = ('user__email', 'user__profile__first_name', 'user__profile__last_name')
+
+    def first_name(self, obj):
+        return obj.user.profile.first_name
+
+    def last_name(self, obj):
+        return obj.user.profile.last_name
