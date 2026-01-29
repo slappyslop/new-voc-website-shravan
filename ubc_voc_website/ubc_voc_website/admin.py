6f5536af0a13c22d4ee828d34452f55c95e130aa
epw1624 <emily.wood1624@gmail.com>
2026-01-28 19:39:28 -0800

    Allow admin to edit user email

--- ubc_voc_website/ubc_voc_website/admin.py
+++ ubc_voc_website/ubc_voc_website/admin.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -17,3 +21,14 @@ class UserAdmin(BaseUserAdmin):
 
     search_fields = ('email', 'old_id')
     ordering = ('email',)
+
+    def save_model(self, request, obj, form, change):
+        new_email = request.POST.get('email', '').lower().strip()
+        old_email = User.objects.get(pk=obj.pk).email if change else None
+        
+        if new_email and new_email != old_email:
+            User.objects.filter(pk=obj.pk).update(email=new_email)
+            EmailAddress.objects.filter(user=obj, primary=True).update(email=new_email)
+            self.message_user(request, f'Email updated to {new_email}', level=messages.SUCCESS)
+        else:
+            super().save_model(request, obj, form, change)
